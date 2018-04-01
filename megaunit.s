d4a67cf31f04143ce20a666725ac6f9d1e2834f9
Jake Snowy <thimil7@gmail.com>
2018-04-01 13:04:00 -0300

    Remove old add

--- megaunit.s
+++ megaunit.s
@@ -7,40 +7,6 @@ section .text
 bits 64
 default rel
 
-; add_asm:
-;     sub rsp, 32
-;     ; num2_sz => RCX -- becames loop_1_counter
-;     ; num3_sz => RDX -- becames loop_2_counter
-;     ; num2*   => R8
-;     ; num3*   => R9
-;     ; R10 => num_idx
-;     ; R11 => temp_guy
-;     xor r10, r10 ; num_idx = 0; carry = 0;
-;     inc rdx
-;     inc rdx
-;     inc rcx
-; loop_1:
-;     dec rdx
-;     dec rcx
-;     jz loop_2 ; if zero goto 2nd loop
-;     mov r11, 0
-;     adc r11, [r8+8*r10]
-;     jz jump_over ; jump over adc
-;     adc [r9+8*r10], r11
-; jump_over:
-;     inc r10
-;     jmp loop_1
-; loop_2:
-;     dec rdx
-;     jz end
-;     mov r11, 0
-;     adc [r9+8*r10], r11
-;     inc r10
-;     jmp loop_2
-; end:
-;     add rsp, 32
-;     ret
-
 add_asm:
     sub rsp, 32
     ; num2_sz => RCX -- becames loop_1_counter
