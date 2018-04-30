7d272b5f4d0d37e849b4d0d0aaadc8a9f5af7b59
jakesnowyy <thimil7@gmail.com>
2018-04-29 21:55:13 -0300

    cross-plaform update

---
+++ configure.py
@@ -0,0 +1,86 @@
+import sys
+import platform
+
+debug = True
+optimizate = False
+test = False
+
+args = sys.argv[1:]
+# if len(args) == 0:
+#     args.append('-h')
+
+for arg in args:
+    if arg == "--test-ok":
+        test = True
+    elif arg == "--debug-ok":
+        debug = True
+        optimizate = False
+    elif arg == "--nodebug-ok":
+        debug = False
+    elif arg == "--release-ok":
+        debug = False
+        optimizate = True
+    elif arg in ["--help", "-h"]:
+        print(
+"""\
+options:
+  --test-ok: enable testing, disabled by default
+  --debug-ok: enable debugging, enabled by default
+  --nodebug-ok: disable debugging
+  --release-ok: no debug and enable optimizations, disabled by default
+  --help or -h: print this message
+be good\
+        """)
+        exit(0)
+
+print('generating makefile...')
+mk = open("makefile", "w")
+options = ""
+if debug:
+    options += "-g"
+if optimizate:
+    options += "-O2"
+if options != "":
+    mk.write("options = " + options + "\n")
+
+os = ""
+if platform.machine().endswith("64"):
+    if platform.system() == "Linux":
+        os = "linux"
+        mk.write("os = linux\n")
+        mk.write("obj_ext = .o\n")
+        mk.write("ASMFLAGS = -felf64 -d" + os + "\n")
+        print('64 bit linux system detected')
+
+    elif platform.system() == "Windows":
+        os = "windwos"
+        mk.write("os = windwos\n")
+        mk.write("exe_ext = .exe\n")
+        mk.write("obj_ext = .obj\n")
+        mk.write("ASMFLAGS = -fwin64 -d" + os + "\n")
+        print('64 bit windows system detected')
+
+    else:
+        print("Sorry, unsupported system")
+        mk.close()
+        exit(1)
+
+f = open("makefile.in", "r")
+mk.write(f.read())
+
+if os == 'linux':
+    mk.write("""
+
+clean:
+\t@rm -f $(TEMPS)\n""")
+elif os == 'windwos':
+    mk.write("""
+
+clean:
+\t@del $(TEMPS) 2>nul\n""")
+
+f.close()
+mk.close()
+print('makefile built')
+
+pass
--- megaunit.s
+++ megaunit.s
@@ -9,35 +9,67 @@ global sbl_asm
 global inc_asm
 global dec_asm
 
+;return register
+%define r_ret rax
+
+;scratch registers
+%define r_scr0 r10
+%define r_scr1 r11
+
+;semi_scratch registers
+%define r_ssc0 r12
+%define r_ssc1 r13
+%define r_ssc2 r14
+%define r_ssc3 r15
+
+%define r_nsc0 rbx
+%define r_mul1 rdx
+
+;argument registers
+%ifdef linux
+    %define r_arg0 rdi
+    %define r_arg1 rsi
+    %define r_arg2 rdx
+    %define r_arg3 rcx
+%endif
+
+%ifdef windwos
+    %define r_arg0 rcx
+    %define r_arg1 rdx
+    %define r_arg2 r8
+    %define r_arg3 r9
+%endif
+
+
 section .text
 bits 64
 default rel
 
 add_asm:
     sub rsp, 32
-    ; num2_sz => RCX -- becames loop_1_counter
-    ; num3_sz => RDX -- becames loop_2_counter
-    ; num2*   => R8
-    ; num3*   => R9
-    ; R10 => num_idx
-    ; R11 => temp_guy
-    xor r10, r10 ; num_idx = 0; carry = 0;
-    inc rdx
-    inc rdx
-    inc rcx
+    ; num2_sz => r_arg0 RCX -- becames loop_1_counter
+    ; num3_sz => r_arg1 RDX -- becames loop_2_counter
+    ; num2*   => r_arg2 R8
+    ; num3*   => r_arg3 R9
+    ; r_scr0 R10 => num_idx
+    ; r_scr1 R11 => temp_guy
+    xor r_scr0, r_scr0 ; num_idx = 0; carry = 0;
+    inc r_arg1
+    inc r_arg1
+    inc r_arg0
 add_loop_1:
-    dec rdx
-    dec rcx
+    dec r_arg1
+    dec r_arg0
     jz add_loop_2 ; if zero goto 2nd loop
-    mov r11, [r8+8*r10]
-    adc [r9+8*r10], r11
-    inc r10
+    mov r_scr1, [r_arg2+8*r_scr0]
+    adc [r_arg3+8*r_scr0], r_scr1
+    inc r_scr0
     jmp add_loop_1
 add_loop_2:
-    dec rdx
+    dec r_arg1
     jz add_end
-    adc qword[r9+8*r10], 0
-    inc r10
+    adc qword[r_arg3+8*r_scr0], 0
+    inc r_scr0
     jmp add_loop_2
 add_end:
     add rsp, 32
@@ -45,29 +77,29 @@ add_end:
 
 sub_asm:
     sub rsp, 32
-    ; num2_sz => RCX -- becames loop_1_counter
-    ; num3_sz => RDX -- becames loop_2_counter
-    ; num2*   => R8
-    ; num3*   => R9
-    ; R10 => num_idx
-    ; R11 => temp_guy
-    xor r10, r10 ; num_idx = 0; carry = 0;
-    inc rdx
-    inc rdx
-    inc rcx
+    ; num2_sz => r_arg0 RCX -- becames loop_1_counter
+    ; num3_sz => r_arg1 RDX -- becames loop_2_counter
+    ; num2*   => r_arg2 R8
+    ; num3*   => r_arg3 R9
+    ; r_scr0 R10 => num_idx
+    ; r_scr1 R11 => temp_guy
+    xor r_scr0, r_scr0 ; num_idx = 0; carry = 0;
+    inc r_arg1
+    inc r_arg1
+    inc r_arg0
 sub_loop_1:
-    dec rdx
-    dec rcx
+    dec r_arg1
+    dec r_arg0
     jz sub_loop_2 ; if zero goto 2nd loop
-    mov r11, [r8+8*r10]
-    sbb [r9+8*r10], r11
-    inc r10
+    mov r_scr1, [r_arg2+8*r_scr0]
+    sbb [r_arg3+8*r_scr0], r_scr1
+    inc r_scr0
     jmp sub_loop_1
 sub_loop_2:
-    dec rdx
+    dec r_arg1
     jz sub_end
-    sbb qword [r9+8*r10], 0
-    inc r10
+    sbb qword [r_arg3+8*r_scr0], 0
+    inc r_scr0
     jmp sub_loop_2
 sub_end:
     add rsp, 32
@@ -75,48 +107,52 @@ sub_end:
 
 mul_asm:
     sub rsp, 32
-    push r12
-    push r13
-    push r14
-    push r15
-    ; vec     => RCX
-    ; num1_sz => RDX
-    ; num2_sz => R8
-    ; R9 => num1_sz
-    ; R10 => num_idx -- num2_sz
-    ; R11 => num_idx_2 -- num1_sz
-    ; R12 => num1*
-    ; R13 => num2*
-    ; RAX => tmp_and_mul
-    ; R14 => tmp
-    ; R15 => tmp_2 -- idx+idx_2
-    mov r9, rdx
-    mov r12, [rcx]
-    mov r13, [rcx+8]
-    mov r10, r8
+    push r_ssc0
+    push r_ssc1
+    push r_ssc2
+    push r_ssc3
+    push r_nsc0
+    ; vec     => r_arg0 RCX
+    ; num1_sz => r_arg1 RDX
+    ; num2_sz => r_arg2 R8
+    ; r_arg3 R9 => num1_sz
+    ; r_scr0 R10 => num_idx -- num2_sz
+    ; r_scr1 R11 => num_idx_2 -- num1_sz
+    ; r_ssc0 R12 => num1*
+    ; r_ssc1 R13 => num2*
+    ; r_ssc2 R14 => tmp
+    ; r_ssc3 R15 => tmp_2 -- idx+idx_2
+    ; r_ret RAX => tmp_and_mul
+    ; this function uses r_mul1, so it cant use 
+    ; r_arg1 nor r_arg2
+    mov r_arg3, r_arg1
+    mov r_ssc0, [r_arg0]
+    mov r_ssc1, [r_arg0+8]
+    mov r_scr0, r_arg2
 loop_out:
-    mov r11, r9
-    dec r10
+    mov r_scr1, r_arg3
+    dec r_scr0
     js mul_end
 loop_in:
-    dec r11
+    dec r_scr1
     js loop_out
-    mov r15, r10
-    add r15, r11
-    mov rax, [r13+8*r10]
-    mul qword [r12+8*r11]
+    mov r_ssc3, r_scr0
+    add r_ssc3, r_scr1
+    mov r_ret, [r_ssc1+8*r_scr0]
+    mul qword [r_ssc0+8*r_scr1]
     jnc jump
-    mov r14, [rcx+8*r10+16]
-    mov [r14+8*r15+8], rdx
+    mov r_ssc2, [r_arg0+8*r_scr0+16]
+    mov [r_ssc2+8*r_ssc3+8], r_mul1
 jump:
-    mov r14, [rcx+8*r10+24]
-    mov [r14+8*r15], rax
+    mov r_ssc2, [r_arg0+8*r_scr0+24]
+    mov [r_ssc2+8*r_ssc3], r_ret
     jmp loop_in
 mul_end:
-    pop r15
-    pop r14
-    pop r13
-    pop r12
+    pop r_nsc0
+    pop r_ssc3
+    pop r_ssc2
+    pop r_ssc1
+    pop r_ssc0
     add rsp, 32
     ret
 
@@ -130,41 +166,40 @@ mul_end:
 
 rsb_asm:
     sub rsp, 32
-    ; num_sz  => RCX
-    ; num     => RDX
+    ; num_sz  => r_arg0 RCX
+    ; num     => r_arg1 RDX
     ; R8 => 
     ; R9 => 
-    ; R10 => num_idx
-    ; R11 => temp_guy
-    xor r10, r10 ; num_idx = 0; carry = 0;
+    ; r_scr0 R10 => num_idx
+    xor r_scr0, r_scr0 ; num_idx = 0; carry = 0;
     ; inc rcx
 rsb_loop:
-    dec rcx
+    dec r_arg0
     jz rsb_end ; if zero goto end
-    rcr qword [rdx+8*rcx], 1
+    rcr qword [r_arg1+8*r_arg0], 1
     jmp rsb_loop
 rsb_end:
-    rcr qword [rdx], 1
+    rcr qword [r_arg1], 1
     add rsp, 32
     ret
 
 inc_asm:
     sub rsp, 32
-    ; num_sz  => RCX
-    ; num     => RDX
+    ; num_sz  => r_arg0 RCX
+    ; num     => r_arg1 RDX
     ; R8 => 
     ; R9 => 
-    ; R10 => num_idx
+    ; r_scr0 R10 => num_idx
     ; R11 => temp_guy
-    xor r10, r10 ; num_idx = 0; carry = 0;
+    xor r_scr0, r_scr0 ; num_idx = 0; carry = 0;
     stc
-    inc rcx
+    inc r_arg0
 inc_loop:
     jnc inc_end
-    dec rcx
+    dec r_arg0
     jz inc_end ; if zero goto end
-    adc qword [rdx+8*r10], 0
-    inc r10
+    adc qword [r_arg1+8*r_scr0], 0
+    inc r_scr0
     jmp inc_loop
 inc_end:
     add rsp, 32
@@ -172,21 +207,21 @@ inc_end:
 
 dec_asm:
     sub rsp, 32
-    ; num_sz  => RCX
-    ; num     => RDX
+    ; num_sz  => r_arg0 RCX
+    ; num     => r_arg1 RDX
     ; R8 => 
     ; R9 => 
-    ; R10 => num_idx
+    ; r_scr0 R10 => num_idx
     ; R11 => temp_guy
-    xor r10, r10 ; num_idx = 0; carry = 0;
+    xor r_scr0, r_scr0 ; num_idx = 0; carry = 0;
     stc
-    inc rcx
+    inc r_arg0
 dec_loop:
     jnc dec_end
-    dec rcx
+    dec r_arg0
     jz dec_end ; if zero goto end
-    sbb qword [rdx+8*r10], 0
-    inc r10
+    sbb qword [r_arg1+8*r_scr0], 0
+    inc r_scr0
     jmp dec_loop
 dec_end:
     add rsp, 32
@@ -194,21 +229,21 @@ dec_end:
 
 sbl_asm:
     sub rsp, 32
-    ; num_sz  => RCX
-    ; num     => RDX
+    ; num_sz  => r_arg0 RCX
+    ; num     => r_arg1 RDX
     ; R8 => 
     ; R9 => 
-    ; R10 => num_idx
+    ; r_scr0 R10 => num_idx
     ; R11 => temp_guy
-    xor r10, r10 ; num_idx = 0; carry = 0;
+    xor r_scr0, r_scr0 ; num_idx = 0; carry = 0;
     ; inc rcx
 sbl_loop:
-    dec rcx
+    dec r_arg0
     jz sbl_end ; if zero goto end
-    rcl qword [rdx+8*r10], 1
-    inc r10
+    rcl qword [r_arg1+8*r_scr0], 1
+    inc r_scr0
     jmp sbl_loop
 sbl_end:
-    rcl qword [rdx+8*r10], 1
+    rcl qword [r_arg1+8*r_scr0], 1
     add rsp, 32
     ret
